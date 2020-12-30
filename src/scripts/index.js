2484b03c01bbddc0fd28cd74620ec2a2e6319368
cunguyendev <cunguyen.dev@gmail.com>
2020-12-30 15:36:46 +0700

    Validation data input.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,41 @@
-console.log('====================================');
-console.log('Hello world!');
-console.log('====================================');
+(function main() {
+  const username = document.querySelector('input[name="username"]');
+  const password = document.querySelector('input[name="password"]');
+  const loginButton = document.querySelector('button[name="btn-login"]');
+  const message = document.querySelector('.actions__message');
+
+  const showYearInTheFooter = () => {
+    const year = document.querySelector('.main-footer__copyright').innerHTML.replace('$year', `${new Date().getFullYear()}`);
+
+    document.querySelector('.main-footer__copyright').innerHTML = year;
+  };
+
+  showYearInTheFooter();
+
+  const validationHandling = () => {
+    let theMessage = '';
+
+    if (!username.value) {
+      theMessage = 'Please enter the username!';
+    }
+
+    if (!password.value) {
+      theMessage = 'Please enter the password!';
+    }
+
+    message.innerHTML = theMessage;
+
+    if (username.value && password.value) {
+      theMessage = '';
+
+      return true;
+    }
+    return false;
+  };
+
+  loginButton.addEventListener('click', () => {
+    const isValidData = validationHandling();
+
+    console.log(username.value, password.value);
+  });
+}());
